219b093dd7ee92acf5c4a65f5092425f085c5dfc
Aioncheg <sch1081013@gmail.com>
2015-04-26 20:33:01 +0500

    add cookie splitterposition

--- _/js/myscript.js
+++ _/js/myscript.js
@@ -1,10 +1,11 @@
 // Horizontal splitter
-var splitterPosition = 60;
+var splitterPosition = $.cookie('splitterposition') || 60;
 var splitter = $('.dictionary-container').split({
 		position: splitterPosition + '%',
 		orientation: 'horizontal',
 		onDragEnd: function() {
 			splitterPosition = Math.round(splitter.position()*100/$('.dictionary-container').height());
+			$.cookie('splitterposition', splitterPosition, {expires: 365});
 		}
 });
 
